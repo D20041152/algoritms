5f759e9f33d27bb8940b085d865affd09c7382e8
Denis Budin <D20041152@yandex.ru>
2024-02-08 01:36:58 +0300

    shell sort

---
+++ shell_sort.py
@@ -0,0 +1,14 @@
+def shell_sort(mas):
+    n = len(mas)
+    step = n // 2
+
+    while step > 0:
+        for i in range(step, n):
+            temp = mas[i]
+            j = i
+            while j >= step and mas[j - step] > temp:
+                mas[j] = mas[j - step]
+                j -= step
+            mas[j] = temp
+        step //= 2
+    return mas
