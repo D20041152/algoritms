a9b17deabbb80a341d9565c118b04c113a0bd9f6
Denis Budin <D20041152@yandex.ru>
2024-02-13 00:58:25 +0300

    shaker sort

---
+++ shaker_sort.py
@@ -0,0 +1,26 @@
+def shaker_sort(mas):
+    flag = True
+    n = len(mas)
+    start_sort = 0
+    end_sort = n
+    while flag:
+        flag = False
+        for i in range(start_sort, end_sort - 1):
+            if mas[i] > mas[i + 1]:
+                mas[i], mas[i + 1] = mas[i + 1], mas[i]
+                flag = True
+
+        if not flag:
+            break
+
+        for i in range(end_sort, start_sort+1):
+            if mas[i] < mas[i + 1]:
+                mas[i + 1], mas[i] = mas[i], mas[i + 1]
+                flag = True
+    start_sort += 1
+    end_sort -= 1
+    return mas
+
+
+mas = [4, 3, 2, 1, 5, 6, 7, 45, 32]
+print(shaker_sort(mas))
